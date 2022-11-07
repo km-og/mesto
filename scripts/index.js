c820160864bed6010da4cabdce3bef1d1bd73864
Inessa Kim <kim.inessa20@yandex.ru>
2022-11-07 14:38:17 +0300

    Ошибки исправлены

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,28 @@
 let editButton = document.querySelector(".profile__edit-button");
 let popup = document.querySelector(".popup");
+let closeButton = document.querySelector(".popup__close-button");
+let namePopup = document.querySelector(".popup_input-name");
+let nameProfile = document.querySelector(".profile__name");
+let descriptionPopup = document.querySelector(".popup_input-description");
+let descriptionProfile = document.querySelector(".profile__description");
+
 function openPopup() {
-  popup.setAttribute("class", "popup popup_opened");
+  popup.classList.add("popup_opened");
+  namePopup.value = nameProfile.textContent;
+  descriptionPopup.value = descriptionProfile.textContent;
 }
 editButton.addEventListener("click", openPopup);
 
-let closeButton = document.querySelector(".popup__close-button");
 function closePopup() {
-  popup.removeAttribute("class");
-  popup.setAttribute("class", "popup");
+  popup.classList.remove("popup_opened");
 }
 closeButton.addEventListener("click", closePopup);
-
-let namePopup = document.querySelector(".popup__name");
-let nameProfile = document.querySelector(".profile__name");
-let descriptionPopup = document.querySelector(".popup__description");
-let descriptionProfile = document.querySelector(".profile__description");
-function rename() {
-  nameProfile.textContent = namePopup.value;
-  descriptionProfile.textContent = descriptionPopup.value;
-}
-rename();
 
 let formElement = document.querySelector(".popup__container");
 function formSubmitHandler(evt) {
   evt.preventDefault();
-  rename();
+  nameProfile.textContent = namePopup.value;
+  descriptionProfile.textContent = descriptionPopup.value;
   closePopup();
 }
 
